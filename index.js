27a5266d15df954e6c786f0a344beea56e85eebe
Alexandr <Alexandr.Florinskij@yandex.ua>
2019-07-19 19:57:56 +0300

    init

---
+++ index.js
@@ -0,0 +1,31 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import { Text } from 'react-native';
+
+const VGSCollectCard = () => {
+  return (
+    <Text>
+      HI There
+    </Text>
+  );
+};
+
+
+export default VGSCollectCard;
+
+// /**
+//  * @format
+//  */
+
+// import {AppRegistry} from 'react-native';
+// import App from './App';
+// import {name as appName} from './app.json';
+
+// AppRegistry.registerComponent(appName, () => App);
